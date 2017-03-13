76bf0a3120aa3c9799fd205e0a36dd02d7069c21
Per Samuelsson <per@starcounter.com>
2017-03-13 14:56:12 +0100

    Improved on pack.bat and consumed version from file rather than .nuspec

--- build/pack.bat
+++ build/pack.bat
@@ -1,3 +1,13 @@
 @echo off
 
-nuget pack -o ..\artifacts Starcounter.VisualStudio.nuspec
+set /p PackVersion=<version.txt
+set OutDir=..\artifacts
+set VSIXFile=..\bin\Release\Starcounter.VS.vsix
+
+:: Make sure content is there, to know we don't produce an empty one
+IF NOT EXIST "%VSIXFile%" (
+	ECHO File not found: %VSIXFile%
+	EXIT /b 991
+)
+
+nuget pack -OutputDirectory %OutDir% -Version %PackVersion% Starcounter.VisualStudio.nuspec
