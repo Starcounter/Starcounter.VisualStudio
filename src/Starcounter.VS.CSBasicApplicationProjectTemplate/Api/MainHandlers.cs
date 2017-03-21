34ed1e4db19017022144a9fc876372c9b3fcb576
Kim Böndergaard <threezool@gmail.com>
2017-03-21 11:43:53 +0100

    Code cleanup

---
+++ src/Starcounter.VS.CSBasicApplicationProjectTemplate/Api/MainHandlers.cs
@@ -0,0 +1,34 @@
+﻿using $safeprojectname$.ViewModels;
+using Starcounter;
+
+namespace $safeprojectname$.Api
+{
+    internal static class MainHandlers
+    {
+        public static void Register()
+        {
+            Application.Current.Use(new HtmlFromJsonProvider());
+            Application.Current.Use(new PartialToStandaloneHtmlProvider());
+
+            Handle.GET("/$safeprojectname$", () =>
+            {
+                var page = new MainPage()
+                {
+                    Visitors =
+                    {
+                        Data = Db.SQL<GuestBookEntry>("SELECT g FROM GuestBookEntry g")
+                    }
+                };
+
+                if (Session.Current == null)
+                {
+                    Session.Current = new Session(SessionOptions.PatchVersioning);
+                }
+
+                page.Session = Session.Current;
+
+                return page;
+            });
+        }
+    }
+}
